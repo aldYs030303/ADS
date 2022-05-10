c279d83b95dc94dde9eec0e494f78bf297450b07
Aldiyar <61989249+aldYs030303@users.noreply.github.com>
2022-05-10 21:55:32 +0600

    Add files via upload

---
+++ BST.java
@@ -0,0 +1,110 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class BST<K extends Comparable<K>, V> {
+    private Node root;
+
+    private class Node {
+        private K key;
+        private V value;
+        private Node left, right;
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "{" + key + " " + value + "}";
+        }
+    }
+
+    public BST() {
+        root = null;
+    }
+
+    public void put(K key, V value) {
+        Node n = new Node(key, value);
+        root = put(root, n);
+    }
+
+    public Node put(Node root, Node n) {
+        if (root == null) {
+            root = new Node(n.key, n.value);
+            return root;
+        }
+
+        if (n.key.compareTo(root.key) < 0)
+            root.left = put(root.left, n);
+        else if (n.key.compareTo(root.key) > 0)
+            root.right = put(root.right, n);
+
+        return root;
+    }
+
+    public V get(K key) {
+        return search(root, key).value;
+    }
+
+    private Node search(Node node, K key) {
+        if (node == null) return new Node(null, null);
+        if (key == node.key) return node;
+        if (key.compareTo(node.key) < 0) return search(node.left, key);
+        if (key.compareTo(node.key) > 0) return search(node.right, key);
+        return new Node(null, null);
+    }
+
+    public Node getMin() {
+        Node min = root;
+        while (min.left != null) {
+            min = min.left;
+        }
+        return min;
+    }
+
+    public Node getMax() {
+        Node max = root;
+        while (max.right != null) {
+            max = max.right;
+        }
+        return max;
+    }
+
+    public void delete(K key) {
+        root = delete(root, key);
+    }
+
+    public Node delete(Node root, K key) {
+        if (root == null) return null;
+
+        if (key.compareTo(root.key) < 0)
+            root.left = delete(root.left, key);
+        else if (key.compareTo(root.key) > 0)
+            root.right = delete(root.right, key);
+        else {
+            if (root.left == null)
+                return root.right;
+            else if (root.right == null)
+                return root.left;
+
+            root.key = getMin().key;
+
+            root.right = delete(root.right, root.key);
+        }
+
+        return root;
+    }
+
+    public Iterable<K> iterator() {
+        PriorityQueue<K> q = new PriorityQueue<K>();
+        inorder(root, q);
+        return q;
+    }
+
+    private void inorder(Node n, PriorityQueue<K> q) {
+        if(n == null) return;
+        inorder(n.left, q);
+        q.remove(n.key);
+        inorder(n.right, q);
+    }
+}
